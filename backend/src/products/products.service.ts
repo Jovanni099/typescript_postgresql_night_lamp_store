72aebc9cd4d7d59417e8d597ce5d9d7598b46665
Jovanni099 <jovanni099tmb@gmail.com>
2026-04-11 08:33:50 -0700

    add product by id route

--- backend/src/products/products.service.ts
+++ backend/src/products/products.service.ts
@@ -2,11 +2,17 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class ProductsService {
+  private products = [
+    { id: 1, name: 'Moon Night Lamp', price: 25 },
+    { id: 2, name: 'Star Night Lamp', price: 30 },
+    { id: 3, name: 'Cloud Night Lamp', price: 28 },
+  ];
+
   findAll() {
-    return [
-      { id: 1, name: 'Moon Night Lamp', price: 25 },
-      { id: 2, name: 'Star Night Lamp', price: 30 },
-      { id: 3, name: 'Cloud Night Lamp', price: 28 },
-    ];
+    return this.products;
+  }
+
+  findOne(id: number) {
+    return this.products.find((product) => product.id === id);
   }
 }
