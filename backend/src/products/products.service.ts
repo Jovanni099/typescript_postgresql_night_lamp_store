5364722aead02c65ff5bc91d90d395214285381d
Jovanni099 <jovanni099tmb@gmail.com>
2026-04-18 01:31:31 -0700

    connect products service to prisma

--- backend/src/products/products.service.ts
+++ backend/src/products/products.service.ts
@@ -1,63 +1,43 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 
 @Injectable()
 export class ProductsService {
-  private products = [
-    { id: 1, name: 'Moon Night Lamp', price: 25 },
-    { id: 2, name: 'Star Night Lamp', price: 30 },
-    { id: 3, name: 'Cloud Night Lamp', price: 28 },
-  ];
+  constructor(private readonly prisma: PrismaService) {}
 
   findAll() {
-    return this.products;
+    return this.prisma.product.findMany({
+      orderBy: { id: 'asc' },
+    });
   }
 
   findOne(id: number) {
-    return this.products.find((product) => product.id === id);
+    return this.prisma.product.findUnique({
+      where: { id },
+    });
   }
 
   create(createProductDto: CreateProductDto) {
-    const newProduct = {
-      id: this.products.length + 1,
-      name: createProductDto.name,
-      price: createProductDto.price,
-    };
-
-    this.products.push(newProduct);
-
-    return newProduct;
+    return this.prisma.product.create({
+      data: {
+        name: createProductDto.name,
+        price: createProductDto.price,
+      },
+    });
   }
 
   update(id: number, updateProductDto: UpdateProductDto) {
-    const product = this.products.find((product) => product.id === id);
-
-    if (!product) {
-      return { message: 'Product not found' };
-    }
-
-    if (updateProductDto.name !== undefined) {
-      product.name = updateProductDto.name;
-    }
-
-    if (updateProductDto.price !== undefined) {
-      product.price = updateProductDto.price;
-    }
-
-    return product;
+    return this.prisma.product.update({
+      where: { id },
+      data: updateProductDto,
+    });
   }
 
   remove(id: number) {
-    const productIndex = this.products.findIndex(
-      (product) => product.id === id,
-    );
-    if (productIndex === -1) {
-      return { message: 'Product not found' };
-    }
-    const deleteProduct = this.products[productIndex];
-    this.products.splice(productIndex, 1);
-
-    return deleteProduct;
+    return this.prisma.product.delete({
+      where: { id },
+    });
   }
 }
