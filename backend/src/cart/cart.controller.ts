f09d95af0e36fa6a83c44e8ea73738f0eca58491
Jovanni099 <jovanni099tmb@gmail.com>
2026-05-07 05:11:53 -0700

    feat(cart): add create cart endpoint

--- backend/src/cart/cart.controller.ts
+++ backend/src/cart/cart.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
+import { CartService } from './cart.service';
 
 @Controller('cart')
-export class CartController {}
+export class CartController {
+  constructor(private cartService: CartService) {}
+
+  @Post()
+  create() {
+    return this.cartService.createCart();
+  }
+}
--- backend/src/cart/cart.service.ts
+++ backend/src/cart/cart.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class CartService {}
+export class CartService {
+  constructor(private prisma: PrismaService) {}
+
+  async createCart() {
+    return this.prisma.cart.create({
+      data: {},
+    });
+  }
+}
